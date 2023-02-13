86d9057a5ea231333f8232dd391d4511a382038b
Frank <f.berrocalazofeif887@mybvc.ca>
2023-02-13 11:40:54 -0600

    Activities created,
    App almost completed, new interface test created.

--- app/src/main/java/com/example/demo2_1_activities/MainActivity.java
+++ app/src/main/java/com/example/demo2_1_activities/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.demo2_1_activities;
 
+import static androidx.core.content.PackageManagerCompat.LOG_TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG =
+            MainActivity.class.getSimpleName();
+
+    public static final String EXTRA_MESSAGE =
+            "com.example.demo2_1_activities.MainActivity.extra.MESSAGE";
+
+    private EditText mMessageEditText;
+
+    public static final int TEXT_REQUEST = 1;
+    private TextView mReplyHeadTextView;
+    private TextView mReplyTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMessageEditText = findViewById(R.id.textSendFirst);
+        mReplyHeadTextView = findViewById(R.id.text_header_reply);
+        mReplyTextView = findViewById(R.id.text_message_reply);
+    }
+
+    public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
+
+        Intent intent = new Intent(this, SecondActivity.class);
+        String message = mMessageEditText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
     }
 }
---
+++ app/src/main/java/com/example/demo2_1_activities/SecondActivity.java
@@ -0,0 +1,36 @@
+package com.example.demo2_1_activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    public static final String EXTRA_REPLY =
+            "com.example.demo2_1_activities.MainActivity.extra.REPLY";
+
+    private EditText mReply;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        mReply = findViewById(R.id.textSendSecond);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.textReceiveSecond);
+        textView.setText(message);
+    }
+
+    public void returnReply(View view) {
+        String reply = mReply.getText().toString();
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK,replyIntent);
+        finish();
+    }
+}
